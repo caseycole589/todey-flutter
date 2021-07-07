75f4b69e2d2f9511777e962e3b40ea03fa03d310
casey cole <casey@capsinc.com>
2021-07-07 16:12:14 -0400

    added bottom sheet modal this looks really nice

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AddTask extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF757575),
+      child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 30, color: Colors.lightBlueAccent),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Add',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.all(20),
+                    backgroundColor: Colors.lightBlueAccent,
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
